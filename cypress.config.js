a07192f6391b54b09cc7a09c4f4331ca62ae2034
Donald Navarro <donaldjnavarro@gmail.com>
2025-08-26 16:55:59 -0400

    HTML reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,29 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'reports',
+    overwrite: false,
+    html: true,
+    json: true,
+    embeddedScreenshots: true,
+  },
+
   e2e: {
-    setupNodeEvents() {
-      // implement node event listeners here
+    setupNodeEvents(on, config) {
+      mochawesome(on);
+      return config;
     },
+    screenshotsFolder: 'reports/screenshots',
   },
 
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
-      viteConfig: './vite.config.js'
+      viteConfig: './vite.config.js',
     },
   },
 });
