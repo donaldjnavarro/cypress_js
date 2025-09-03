f78805d439275fced292897086561bd4f803cdc6
Donald Navarro <donaldjnavarro@gmail.com>
2025-09-03 09:16:38 -0400

    Removed unused import

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
